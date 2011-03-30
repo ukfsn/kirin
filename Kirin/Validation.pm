bc4875c13c3ee5d259ef9d5feffb3ee99047b9ab
Jason Clifford <jason@ukfsn.org>
2011-03-30 18:07:53 +0100

    list validation type

--- Kirin/Validation.pm
+++ Kirin/Validation.pm
@@ -16,6 +16,10 @@ our %validations = (
     'Number' => sub {
         return $_[0] =~ /^$RE{num}{real}$/ ? 1 : 0;
     },
+    'List' => sub {
+        my %list = map { $_ => 1 } split(/,/, $_[1];
+        return defined $lists{$_[0]} ? 1 : 0;
+    }
     'UK Postcode' => sub {
         return $_[0] =~  /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/ ? 1 : 0;
     },
